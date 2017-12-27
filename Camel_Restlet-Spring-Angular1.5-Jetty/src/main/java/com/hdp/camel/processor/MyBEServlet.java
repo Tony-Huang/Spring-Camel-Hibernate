69ce6b5d25b6d39ac1f69fd172f6bf908f48cc6c
Tony Huang <hdp214@163.com>
2017-12-27 15:17:24 +0800

    clean code for servlet demo

--- Camel_Restlet-Spring-Angular1.5-Jetty/src/main/java/com/hdp/camel/processor/MyBEServlet.java
+++ Camel_Restlet-Spring-Angular1.5-Jetty/src/main/java/com/hdp/camel/processor/MyBEServlet.java
@@ -17,110 +17,113 @@ import java.net.URLDecoder;
  */
 public class MyBEServlet extends HttpServlet {
 
-        public static int COUNT = 0;
+    public static int COUNT = 0;
 
-        public MyBEServlet() {
+    public MyBEServlet() {
+    }
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+    }
+
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.process(request, response);
+    }
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.process(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.process(request, response);
+    }
+
+    public static String Param_Name_Delay = "timeDelay";
+    public static int BUFFER_SIZE=1024;
+
+    private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println();
+        System.out.println("=====================Backend servlet==================================");
+        String method = request.getMethod();
+        String contextPath = request.getContextPath();
+        String pathInfo = request.getPathInfo();
+        String pathTranslated = request.getPathTranslated();
+        String queryString = request.getQueryString();
+        String requestUri = request.getRequestURI();
+        String requestUrl = request.getRequestURL().toString();
+        String servletPath = request.getServletPath();
+        String contentType = request.getContentType();
+        int contentLength = request.getContentLength();
+        Map parameters = request.getParameterMap();
+
+        String timeDelay = request.getParameter(Param_Name_Delay);
+        System.out.println("schema=" + request.getScheme());
+        System.out.println("method=" + method);
+        System.out.println("contextPath=" + contextPath);
+        System.out.println("queryString=" + queryString +
+                " ; after decoded ,queryString=" + URLDecoder.decode(queryString, "utf-8"));
+        System.out.println("parameters=" + parameters);
+        System.out.println("contentType=" + contentType);
+        System.out.println("contentLength=" + contentLength);
+        System.out.println("pathInfo=" + pathInfo);
+        System.out.println("pathTranslated=" + pathTranslated);
+        System.out.println("requestUri=" + requestUri);
+        System.out.println("requestUrl=" + requestUrl);
+        System.out.println("servletPath=" + servletPath);
+        System.out.println("contentEncoding=" + request.getCharacterEncoding());
+
+        //whether to delay sometime
+        if (timeDelay != null && !timeDelay.isEmpty()) {
+            int sleep = Integer.parseInt(timeDelay);
+            //Thread.currentThread().sleep(sleep);
+            System.out.println("...... sleep end " + "......");
+
         }
 
-        public void init(ServletConfig config) throws ServletException {
-            super.init(config);
+        System.out.println("------");
 
+
+        //read the request body
+        StringBuffer body = new StringBuffer();
+
+        byte[] content = new byte[BUFFER_SIZE];
+        for (int size; (size=request.getInputStream().read(content)) > 0;) {
+            System.out.println("read some body bytes length="+size);
+            body.append(new String(content, "utf-8"));
         }
 
-        protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            this.process(request,response);
-        }
+        System.out.println("request body.length = " + body.length() +" ,body="+body);
 
 
-         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-             this.process(request,response);
-        }
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.setDateHeader("Date", new Date().getTime());
 
-         protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-             this.process(request,response);
-         }
-
-         public static String Param_Name_Delay="timeDelay";
-
-         private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-             System.out.println();
-             System.out.println("=====================Backend servlet==================================" );
-             String method = request.getMethod();
-             String contextPath = request.getContextPath();
-             String pathInfo = request.getPathInfo();
-             String pathTranslated = request.getPathTranslated();
-             String queryString =request.getQueryString();
-             String requestUri = request.getRequestURI();
-             String requestUrl= request.getRequestURL().toString();
-             String servletPath = request.getServletPath();
-             String contentType = request.getContentType();
-             int contentLength = request.getContentLength();
-             Map parameters =request.getParameterMap();
-
-             String timeDelay = request.getParameter(Param_Name_Delay);
-             System.out.println("schema="+request.getScheme());
-             System.out.println("method=" + method );
-             System.out.println("contextPath=" + contextPath );
-             System.out.println("queryString=" + queryString +
-                     " ; after decoded ,queryString="+URLDecoder.decode(queryString,"utf-8"));
-             System.out.println("parameters=" + parameters );
-             System.out.println("contentType=" + contentType );
-             System.out.println("contentLength=" + contentLength );
-             System.out.println("pathInfo=" + pathInfo );
-             System.out.println("pathTranslated=" + pathTranslated );
-             System.out.println("requestUri=" + requestUri );
-             System.out.println("requestUrl=" + requestUrl );
-             System.out.println("servletPath=" + servletPath );
-             System.out.println("contentEncoding="+request.getCharacterEncoding());
-
-             //whether to delay sometime
-             if (timeDelay != null && !timeDelay.isEmpty()) {
-                 int sleep = Integer.parseInt(timeDelay);
-                 try {
-                     System.out.println("......to seep for "+sleep +" milliseconds" +"......");
-                   Thread.currentThread().sleep(sleep);
-                     System.out.println("...... sleep end "+"......");
-
-                 } catch (Exception e) {
-                     e.printStackTrace();
-                 }
-
-             }
-
-             System.out.println("------");
-
-             int available = request.getInputStream().available();
-             if (available > 0) {
-                 System.out.println("available body data length=" + available );
-
-                 StringBuffer body = new StringBuffer();
-
-                 byte[] content = new byte[available];
-                 int readResult = request.getInputStream().read(content,0,1024);
-                 while( readResult > 0 ) {
-                     body.append(new String(content,"utf-8"));
-                     readResult = request.getInputStream().read(content,readResult,1024);
-                 }
-
-                 System.out.println("body = "+body);
-             }
+        long time = new Date().getTime();
+        response.getWriter().println(
+                " { \"count\" " + ":" + (COUNT++)
+                        + "," + " \"date\" " + ":" + time
+                        + "}"
+        );
 
 
-             response.setContentType("application/json");
-             response.setCharacterEncoding("utf-8");
-             response.setDateHeader("Date",new Date().getTime());
+        //end
+        System.out.println("=====================Backend servlet end!==================================");
+        System.out.println();
+        //myMethod();
+    }
 
-             long time = new Date().getTime();
-             response.getWriter().println(
-                     " { \"count\" "+ ":"+ (COUNT++)
-                        + "," + " \"date\" "+ ":" +time
-                             +"}"
-             );
+    private void myMethod(HttpServletRequest request) {
 
+        System.out.println( "servletContext="+ this.getServletContext() );
+        System.out.println( "servletConfig="+ this.getServletConfig() );
+        System.out.println("init params="+this.getServletConfig().getInitParameterNames());
+        System.out.println("init params.desc="+this.getServletConfig().getInitParameter("desc"));
+        System.out.println("servlet name="+this.getServletConfig().getServletName());
+       // this.getLastModified(request);
 
-             //end
-             System.out.println("=====================Backend servlet end!==================================" );
-             System.out.println();
-        }
+    }
 
 }
